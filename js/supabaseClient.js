48ff370f19421347a402db7cc91622a1da1cb173
Chizsa <166405662+chizsaleei@users.noreply.github.com>
2025-10-04 22:58:24 +0800

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,1 +1,5 @@
+// Fill with your real values from Supabase Settings → API
+const PROJECT_URL = "https://gwlwctytwendsixwvnvu.supabase.co";
+const ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3bHdjdHl0d2VuZHNpeHd2bnZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NzM2ODQsImV4cCI6MjA3NTE0OTY4NH0.0fg92ZPHU4EoLYv6KDxZzlS12y_y54qRfSQ6CPAHhpo";
+window.supabase = supabase.createClient(PROJECT_URL, ANON_KEY);
 
